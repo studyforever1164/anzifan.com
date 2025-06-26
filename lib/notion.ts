129b083e323cd3728dafc4c9031bbbc6a93e9c36
studyforever1164 <czhen069@gmail.com>
2025-06-26 21:02:13 +0800

    Update notion.ts

--- lib/notion.ts
+++ lib/notion.ts
@@ -2,7 +2,7 @@ import { Client } from '@notionhq/client'
 import { Post } from './types'
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
-const databaseId = process.env.NOTION_DATABASE_ID || '21e598ac3de180c785e5c62ca0949244'
+const databaseId = process.env.NOTION_DATABASE_ID || '21e598ac3de18095a070f77c926e68e8'
 
 // export const getBasicDBInfo = async () => {
 //     const dbQuery: any = {
